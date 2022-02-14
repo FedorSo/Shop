79e050813423ec43cbbed20cb3b60ef6f6a068b4
Fedor <fedya.sorokin.11@mail.ru>
2022-02-14 15:55:04 +0700

    Cделали функцию тех. поддержки,и секцию оплаты

---
+++ script.js
@@ -0,0 +1,9 @@
+function print_contact(){
+
+    const skype= "helper";
+    const telegram= "@helper";
+
+    alert(`Skype: ${skype} \nTelegram: ${telegram} \nТел. 8901006029`);
+
+    console.log("Пользователь обратилься в техническую поддержку");
+}
