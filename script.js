48dff9defb438fd9b9b295daa7bed1816a4f1c28
Fedor <fedya.sorokin.11@mail.ru>
2022-03-18 15:11:19 +0700

    Добавили модальное окно

--- script.js
+++ script.js
@@ -8,9 +8,28 @@ function print_contact(){
     console.log("Пользователь обратилься в техническую поддержку");
 }
 
-function free_prize(){
+// function free_prize(){
 
-    alert("Поздравляем!\nВы получили подарок\nПерейдите по этой ссылке https://www.minecraft.net/ru-ru/download\nЧтобы забрать подарок");
+//     alert("Поздравляем!\nВы получили подарок\nПерейдите по этой ссылке https://www.minecraft.net/ru-ru/download\nЧтобы забрать подарок");
 
-    console.log("Пользователь взял подарок");
+//     console.log("Пользователь взял подарок");
+// }
+
+const modal = document.getElementById("myModal");
+const openBtn = document.getElementById("myBtn");
+const closeBtn = document.getElementById("close");
+
+openBtn.onclick = function (){
+    modal.style.display = "block";
+    console.log("Модальное окно");
+}
+
+closeBtn.onclick = function (){
+    modal.style.display = "none";
+}
+
+window.onclick = function (event){
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
 }
